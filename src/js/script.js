cc16efe3d150a6fe46521887f7d916819ecf54c4
iHeslop <iheslop@optusnet.com.au>
2024-03-15 16:58:55 +1100

    Updated Loading Page, Animations and Themes

--- src/js/script.js
+++ src/js/script.js
@@ -14,6 +14,7 @@ function displayText(txt) {
       }, 2e3));
 }
 
+// Navigation Functionality
 let sections = document.querySelectorAll("section");
 let navLinks = document.querySelectorAll(".nav-links-list-item");
 
@@ -38,3 +39,71 @@ let observer = new IntersectionObserver((entries, observer) => {
 sections.forEach((section) => {
   observer.observe(section);
 });
+
+// Enter loading screen
+function delay(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+async function enter() {
+  const enterView = document.querySelector(".enterView"),
+    text1 = enterView.querySelector(".enterView-text__1"),
+    text2 = enterView.querySelector(".enterView-text__2");
+
+  await delay(1500);
+
+  text1.style.transition = "opacity 0.8s";
+  text2.style.transition = "opacity 0.8s";
+  enterView.style.transition = "opacity 1.2s";
+
+  text1.style.opacity = 0;
+  text2.style.opacity = 0;
+  enterView.style.opacity = 0;
+
+  setTimeout(() => {
+    enterView.remove();
+  }, 1000);
+}
+
+document.addEventListener("DOMContentLoaded", enter);
+
+//Change Theme
+
+let isDarkTheme = false;
+
+function changeTheme(theme) {
+  const root = document.documentElement;
+  const alpine = document.querySelector(".exp-alpine");
+  const css = document.querySelector(".exp-css");
+  const docker = document.querySelector(".exp-docker");
+  const git = document.querySelector(".exp-git");
+  const html = document.querySelector(".exp-html");
+  const js = document.querySelector(".exp-js");
+  const node = document.querySelector(".exp-node");
+  const react = document.querySelector(".exp-react");
+  if (theme === "light" && isDarkTheme) {
+    root.style.setProperty("--c-bg", "#ece7e1");
+    root.style.setProperty("--c-txt", "#1a1818");
+    alpine.src = "/assets/tech/alpine-black.png";
+    css.src = "/assets/tech/css-black.png";
+    docker.src = "/assets/tech/docker-black.png";
+    git.src = "/assets/tech/git-black.png";
+    html.src = "/assets/tech/html-black.png";
+    js.src = "/assets/tech/javascript-black.png";
+    node.src = "/assets/tech/nodejs-black.png";
+    react.src = "/assets/tech/reactjs-black.png";
+    isDarkTheme = false;
+  } else if (theme === "dark" && !isDarkTheme) {
+    root.style.setProperty("--c-bg", "#1a1818");
+    root.style.setProperty("--c-txt", "#ece7e1");
+    alpine.src = "/assets/tech/alpine-white.png";
+    css.src = "/assets/tech/css-white.png";
+    docker.src = "/assets/tech/docker-white.png";
+    git.src = "/assets/tech/git-white.png";
+    html.src = "/assets/tech/html-white.png";
+    js.src = "/assets/tech/javascript-white.png";
+    node.src = "/assets/tech/nodejs-white.png";
+    react.src = "/assets/tech/reactjs-white.png";
+    isDarkTheme = true;
+  }
+}
